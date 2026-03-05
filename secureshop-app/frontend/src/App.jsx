2a5da4a179f2ecbca0c159dc448b58147ac7aec8
ECOLE-IT-2025 <maxence.cuingnet@ecole-it.com>
2026-03-05 14:06:12 +0100

    Ajouter composant React principal

---
+++ secureshop-app/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SecureShop</h1>
+      <p>Application e-commerce - Frontend à développer</p>
+      {/* TODO: Implémenter l'interface utilisateur */}
+    </div>
+  );
+}
+
+export default App;
